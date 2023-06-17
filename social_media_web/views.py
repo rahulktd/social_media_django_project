82cec1fa625f09b6e771a38effad56f658d52512
rahulktd <rahulkpktd@gmail.com>
2023-06-17 10:44:57 +0530

    User suggestion option.

--- social_media_web/views.py
+++ social_media_web/views.py
@@ -1,3 +1,4 @@
+import random
 from itertools import chain
 
 from django.contrib import messages, auth
@@ -30,11 +31,31 @@ def index(request):
     all_users = User.objects.all()
     user_following_all = []
 
-    for user in user_following_all():
-        user_list = User.objects.all()
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
         user_following_all.append(user_list)
 
-    return render(request, 'index.html',{"user_profile":user_profile, 'posts':feed_list})
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestion_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestion_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestion_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user = ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+
+
+    return render(request, 'index.html',{"user_profile":user_profile, 'posts':feed_list,'suggestions_username_profile_list':suggestions_username_profile_list[:4]})
 def signup(request):
 
     if request.method == 'POST':
